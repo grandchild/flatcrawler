a3562c4fc345d7c2a39c5208a3d9d2c4f67ae163
jakob <grandchild@gmx.net>
2019-02-15 16:06:47 +0100

    Remove old loop comment

--- crawler.py
+++ crawler.py
@@ -210,7 +210,6 @@ WantedBy=timers.target'''
     print(timer_file)
 
 
-'''Check in a loop until SIGTERM.'''
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == 'service':
