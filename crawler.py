f7515443ee980efcc1fdb744f68c545c9b433b84
jakob <grandchild@gmx.net>
2019-02-28 20:47:33 +0100

    Run service in project dir, not user home

--- crawler.py
+++ crawler.py
@@ -199,6 +199,7 @@ After=network-online.target nss-lookup.target
 [Service]
 Type=simple
 ExecStart=/usr/bin/python3 -u {os.path.realpath(__file__)}
+WorkingDirectory={os.path.realpath(__file__)}
 
 [Install]
 WantedBy=multi-user.target"""
